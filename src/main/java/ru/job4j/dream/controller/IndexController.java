42f1f83d9f7d58ab773ffd6bcb957570fa865f9b
dmitryjob4j <haoos@inbox.ru>
2022-03-28 17:12:10 +0300

    6.HTML Link[#504843]

---
+++ src/main/java/ru/job4j/dream/controller/IndexController.java
@@ -0,0 +1,20 @@
+package ru.job4j.dream.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 3.2.2. Html, Bootstrap, Thymeleaf
+ * 6. HTML Link [#504843]
+ * IndexController. Контроллер.
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 28.03.2022
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
