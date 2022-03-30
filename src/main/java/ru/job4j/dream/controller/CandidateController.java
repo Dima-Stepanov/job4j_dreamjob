e0f5686abaf0aa8bd3f023ec736fc84fe6a9dfca
dmitryjob4j <haoos@inbox.ru>
2022-03-30 12:34:57 +0300

    8.HTML form. Создание кандидата.[#504845]

--- src/main/java/ru/job4j/dream/controller/CandidateController.java
+++ src/main/java/ru/job4j/dream/controller/CandidateController.java
@@ -3,12 +3,14 @@ package ru.job4j.dream.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.store.CandidateStore;
 
 /**
  * 3.2.2. Html, Bootstrap, Thymeleaf
  * 5. Список Вакансий [#504842]
  * CandidateController контроллер.
+ * 8. HTML form. Создание кандидата. [#504845]
  *
  * @author Dmitry Stepanov, user Dmitry
  * @since 28.03.2022
@@ -21,5 +23,12 @@ public class CandidateController {
     public String candidates(Model model) {
         model.addAttribute("candidates", candidateStore.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/addCandidate")
+    public String addCandidate(Model model) {
+        model.addAttribute("candidate",
+                new Candidate(0, "Java developer", "Java developer 1000$"));
+        return "addCandidate";
     }
 }
